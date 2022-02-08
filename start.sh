d47a62511840c5f762b090d37f0d25d54ae91c62
200920082007 <93928612+200920082007@users.noreply.github.com>
2022-02-08 18:59:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/josprojects/tgmoviebot.git /tgmoviebot
+git clone -b master https://github.com/200920082007/tgmoviebot.git /tgmoviebot
 cd /tgmoviebot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
+echo "Starting Tiger Shroff, Please Wait..."
 python3 bot.py
