c25531f18dff1c24c9d729fe1647f2299837e193
200920082007 <93928612+200920082007@users.noreply.github.com>
2022-02-09 10:47:15 +0530

    Update script.py

--- script.py
+++ script.py
@@ -124,23 +124,7 @@ Example:
 2. Make sure that your channel does not contains camrips, porn and fake files.
 3. Forward the last message to me with quotes.
  I'll add all the files in that channel to my db."""
-    APPROVAL_TXT = """Here is the help for the Approvals module:
-
-Sometimes, you might trust a user not to send unwanted content.
-Maybe not enough to make them admin, but you might be ok with locks, blacklists, and antiflood not applying to them.
-That's what approvals are for - approve of trustworthy users to allow them to send
-
-<b>User commands:</b>
-× /approval: Check a user's approval status in this chat.
-
-<b>Admin Commands:</b>
-× /approve: Approve of a user. Locks, blacklists, and antiflood won't apply to them anymore.
-× /unapprove: Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
-× /approved: List all approved users.
-
-<b>Owner Commands:</b>
-× /unapproveall: Unapprove ALL users in a chat. This cannot be undone."""
-
+    
     CONNECTION_TXT = """Help: <b>Connections</b>
 
 - Used to connect bot to PM for managing filters 
