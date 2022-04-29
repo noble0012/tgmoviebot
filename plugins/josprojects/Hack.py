cd09264131662880158b7c3d12852012b34c5771
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+200920082007@users.noreply.github.com>
2022-04-29 09:31:20 +0530

    Update Hack.py

--- plugins/josprojects/Hack.py
+++ plugins/josprojects/Hack.py
@@ -1,44 +1,48 @@
-from telethon import events
+import asyncio
 from pyrogram import Client,filters
 
-import asyncio
 
-
-
-@Client.on_message(filters.command(["data", "hack"]))
-async def _(event):
-
-    if event.fwd_from:
-
-        return
-
-    animation_interval = 2
-
-    animation_ttl = range(0, 11)
-
-    input_str = event.pattern_match.group(1)
-
-    if input_str == "hack":
-
-        await event.edit(input_str)
-
-        animation_chars = [
-        
-            "`Connecting To Hacked Private Server...`",
-            "`Target Selected.`",
-            "`Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",    
-            "`Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
-            "`Hacking... 100%\n█████████HACKED███████████ `",
-            "`Targeted Account Hacked...\n\nPay 69$ To` @A_Dark_Princ3 `Or send nudes of female Homo Sapiens To Remove This Hack`"
-        ]
-
-        for i in animation_ttl:
-
-            await asyncio.sleep(animation_interval)
-
-            await event.edit(animation_chars[i % 11])
+@Client.on_message(filters.command(["hack", "heck"]))
+async def hack_func(message):
+    user = await message.client.get_user_dict(message.from_user.id)
+    heckerman = user['mention']
+    animation_chars = [
+        "```Connecting To Private Server \\```",
+        "```Connecting To Private Server |```",
+        "```Connecting To Private Server /```",
+        "```Connecting To Private Server \\```",
+        "```Connection Established ```",
+        "```Target Selected```",
+        "```Backdoor Found In Target```",
+        "```Trying To Hack```",
+        "```Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒```",
+        "```Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒```",
+        "```Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒```",
+        "```Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒```",
+        "```Hacking... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒```",
+        "```Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒```",
+        "```Hacking... 70%\n█████████████████▒▒▒▒▒```",
+        "```Hacking... 88%\n█████████████████████▒```",
+        "```Hacking... 100%\n███████████████████████```",
+        "```Preparing Data... 1%\n▒██████████████████████```",
+        "```Preparing Data... 14%\n████▒██████████████████```",
+        "```Preparing Data... 30%\n████████▒██████████████```",
+        "```Preparing Data... 55%\n████████████▒██████████```",
+        "```Preparing Data... 72%\n████████████████▒██████```",
+        "```Preparing Data... 88%\n████████████████████▒██```",
+        "```Prepared Data... 100%\n███████████████████████```",
+        "```Uploading Data to Server... 12%\n███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒```",
+        "```Uploading Data to Server... 44%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒```",
+        "```Uploading Data to Server... 68%\n███████████████▒▒▒▒▒▒▒▒```",
+        "```Uploading Data to Server... 89%\n████████████████████▒▒▒```",
+        "```Uploaded Data to Server... 100%\n███████████████████████```",
+        "**User Data Upload Completed:** Target's User Data Stored "
+        "at `downloads/victim/telegram-authuser.data.sql`",
+    ]
+    hecked = (f"**Targeted Account Hacked**\n\n```Pay 69$ To``` {heckerman}``` "
+              "To Remove This Hack```")
+    max_ani = len(animation_chars)
+    for i in range(max_ani):
+        await asyncio.sleep(2)
+        await message.edit(animation_chars[i % max_ani])
+    await message.edit(hecked)
